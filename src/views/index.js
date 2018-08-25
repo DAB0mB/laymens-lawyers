6ebbd97b92ca150f6248b5fb425170ffa7a9334f
DAB0mB <emanor6@gmail.com>
2018-08-25 12:22:48 +0800

    appfairy: Migrate

---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+exports.UnauthorizedView = require('./UnauthorizedView')
+exports.NotFoundView = require('./NotFoundView')
+exports.IndexView = require('./IndexView')
+exports.ThankYouView = require('./ThankYouView')
+
+/* eslint-enable */

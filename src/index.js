b6b4a240b13ef5c7dde218f484b8890af20f45fb
DAB0mB <emanor6@gmail.com>
2018-08-25 12:47:44 +0800

    Finalize project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import './styles'
 import './scripts'
-import IndexView from './views/IndexView'
+import IndexController from './controllers/IndexController'
+import ThankYouController from './controllers/ThankYouController'
 
-ReactDOM.render(<IndexView />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={IndexController} />
+      <Route exact path="/thank-you" component={ThankYouController} />
+    </Switch>
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
